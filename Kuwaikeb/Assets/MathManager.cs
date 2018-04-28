0cf49e690c69fdd932682ee225397fb7ea0f2b01
Nav <navya.suri13@gmail.com>
2018-04-28 15:53:39 +0400

    added math scene things

--- Kuwaikeb/Assets/MathManager.cs
+++ Kuwaikeb/Assets/MathManager.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class MathManager : MonoBehaviour {
 
+	public static string mainMode;
+	public static char mainOp;
+	public static int mainTried;
+
+	int[] boundaries = new int[10]{0,9,10,19,20,49,50,99,100,999};
+	char[] opLevel = new char[4]{'+','-','*','/'};
+
+	int levelMark, totalLevel;
+
 	// Use this for initialization
 	void Start () {
-		
+			
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+	public void generate(string mode, char op){
+		int tried = 0;
+		if(mode == "Selective") {
+			levelMark = 1;
+			totalLevel = 7;
+			tried = 5;
+
+		} else {
+			levelMark = 3;
+			totalLevel = 15;
+
+		}
+	}
+
 }
